46939e690aa1b7eb09d149ef0472df1c4526bb4d
Filipe Bezerra <filipe@filipebezerra.tk>
2014-07-08 13:39:08 -0300

    Criada activity que renderiza o web browser direcionando para o site da RMTC. Algumas configurações foram aplicadas para melhor experiência do usuário.

--- app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/MainActivity.java
+++ app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/MainActivity.java
@@ -4,16 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +29,6 @@ public class MainActivity extends ActionBarActivity {
                     .commit();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,6 +45,16 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && (webView.canGoBack())) {
+            webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
     public static class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
@@ -51,7 +64,28 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+            webView = (WebView) rootView.findViewById(R.id.webView);
+            webView.getSettings().setAppCacheEnabled(true);
+
+            // Habilitando suporte JavaScript
+            webView.getSettings().setJavaScriptEnabled(true);
+
+            // Habilitando controles de zoom
+            webView.getSettings().setBuiltInZoomControls(true);
+
+            // Habilitando o clique em links para serem abertos pela própria aplicação e não
+            // pelo aplicativo browser padrão do dispositivo
+            webView.setWebViewClient(new WebViewClient());
+
+            webView.getSettings().setSupportZoom(true);
+            webView.getSettings().setGeolocationEnabled(true);
+
+            webView.loadUrl(rootView.getResources().getString(R.string.rmtc_mob_site));
+
             return rootView;
         }
+
     }
 }
