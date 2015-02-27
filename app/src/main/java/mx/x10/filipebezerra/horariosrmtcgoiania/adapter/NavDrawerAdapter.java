0fddaf02d3dcf6d17a5887cb6197484c6c499884
Filipe Bezerra <filipebzerra@gmail.com>
2015-02-27 04:14:50 -0300

    Navigation Drawer seguindo as recomendações métricas do Material Design.

--- app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/adapter/NavDrawerAdapter.java
+++ app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/adapter/NavDrawerAdapter.java
@@ -8,8 +8,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.readystatesoftware.viewbadger.BadgeView;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,38 +23,39 @@ import mx.x10.filipebezerra.horariosrmtcgoiania.model.NavMenuSection;
  * Navigation drawer adapter specialized for the static menu items.
  *
  * @author Filipe Bezerra
- * @since 2.0
+ * @version 2.0, 02/27/2015
+ * @since #
  */
 public class NavDrawerAdapter extends ArrayAdapter<NavDrawerItem> implements Serializable {
 
     private LayoutInflater mInflater;
 
-    public NavDrawerAdapter(Context context, int textViewResourceId, List<NavDrawerItem> objects ) {
+    public NavDrawerAdapter(Context context, int textViewResourceId, List<NavDrawerItem> objects) {
         super(context, textViewResourceId, objects);
         mInflater = LayoutInflater.from(context);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view = null ;
+        View view = null;
 
         NavDrawerItem menuItem = this.getItem(position);
 
-        if ( menuItem.getType() == NavMenuItem.ITEM_TYPE ) {
-            view = getItemView(convertView, parent, menuItem );
+        if (menuItem.getType() == NavMenuItem.ITEM_TYPE) {
+            view = getItemView(convertView, parent, menuItem);
         } else {
             view = getSectionView(convertView, parent, menuItem);
         }
-        return view ;
+        return view;
     }
 
-    public View getItemView( View convertView, ViewGroup parentView, NavDrawerItem navDrawerItem ) {
+    public View getItemView(View convertView, ViewGroup parentView, NavDrawerItem navDrawerItem) {
         View view = convertView;
         NavMenuItemHolder holder;
-        NavMenuItem menuItem = (NavMenuItem) navDrawerItem ;
+        NavMenuItem menuItem = (NavMenuItem) navDrawerItem;
 
         if (view == null) {
-            view = mInflater.inflate( R.layout.navdrawer_left_items, parentView, false);
+            view = mInflater.inflate(R.layout.navdrawer_left_items, parentView, false);
             holder = new NavMenuItemHolder(view);
             view.setTag(holder);
         } else {
@@ -71,25 +70,20 @@ public class NavDrawerAdapter extends ArrayAdapter<NavDrawerItem> implements Ser
 
         if (holder.counterView != null) {
             if (menuItem.isCounterVisible()) {
-                final BadgeView badgeView = new BadgeView(getContext(), holder.counterView);
-                badgeView.setText(menuItem.getCount());
-                badgeView.setBadgePosition(BadgeView.POSITION_CENTER);
-                badgeView.setBadgeBackgroundColor(getContext().getResources()
-                        .getColor(R.color.badge_counter_background));
-                badgeView.show();
+                holder.counterView.setText(menuItem.getCount());
             } else {
                 holder.counterView.setVisibility(View.GONE);
             }
         }
 
-        return view ;
+        return view;
     }
 
     public View getSectionView(View convertView, ViewGroup parentView,
                                NavDrawerItem navDrawerItem) {
         View view = convertView;
         NavMenuSectionHolder holder;
-        NavMenuSection menuSection = (NavMenuSection) navDrawerItem ;
+        NavMenuSection menuSection = (NavMenuSection) navDrawerItem;
 
         if (view == null) {
             view = mInflater.inflate(R.layout.navdrawer_section, parentView, false);
@@ -123,7 +117,7 @@ public class NavDrawerAdapter extends ArrayAdapter<NavDrawerItem> implements Ser
     public List<NavDrawerItem> getAll() {
         final List<NavDrawerItem> items = new ArrayList<>();
 
-        for(int i = 0; i < getCount(); i++) {
+        for (int i = 0; i < getCount(); i++) {
             items.add(getItem(i));
         }
         return items;
