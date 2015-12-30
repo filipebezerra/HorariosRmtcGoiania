6b8010c1d7f56c2f8bbd7abad882e61d632a4459
filipebezerra <filipebzerra@gmail.com>
2015-12-30 13:55:58 -0200

    Otto already provides an enforcement mechanism to ensure you are always called on main thread.

--- app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/eventbus/BusProvider.java
+++ app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/eventbus/BusProvider.java
@@ -1,9 +1,7 @@
 package mx.x10.filipebezerra.horariosrmtcgoiania.eventbus;
 
-import android.os.Handler;
-import android.os.Looper;
-
 import com.squareup.otto.Bus;
+import com.squareup.otto.ThreadEnforcer;
 
 /**
  * Event bus provider (singleton class).
@@ -14,20 +12,14 @@ import com.squareup.otto.Bus;
  * @see <a href="https://guides.codepath.com/android/Communicating-with-an-Event-Bus">Communicating with an Event Bus</a>
  */
 public class BusProvider {
-    private static final Bus BUS = new Bus();
+    private static final Bus BUS = new Bus(ThreadEnforcer.MAIN);
 
     private BusProvider() {
         // no instances
     }
 
-    private static final Handler mainThread = new Handler(Looper.getMainLooper());
-
-    public static void postOnMain(final Object event) {
-        if (Looper.myLooper() == Looper.getMainLooper()) {
-            BUS.post(event);
-        } else {
-            mainThread.post(() -> BUS.post(event));
-        }
+    public static void post(final Object event) {
+        BUS.post(event);
     }
 
     public static void register(Object obj) {
