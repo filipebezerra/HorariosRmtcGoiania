8f460a6bbfb27b97ac5ad812a855188a455e548c
Filipe Bezerra <filipebzerra@gmail.com>
2015-12-31 15:50:58 -0200

    Enabling logging in application.

---
+++ app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/application/App.java
@@ -0,0 +1,26 @@
+package mx.x10.filipebezerra.horariosrmtcgoiania.application;
+
+import android.app.Application;
+import mx.x10.filipebezerra.horariosrmtcgoiania.BuildConfig;
+import timber.log.Timber;
+
+/**
+ * Application class.
+ *
+ * @author Filipe Bezerra
+ * @version 0.3.0, 31/12/2015
+ * @since 0.3.0
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        enableLogging();
+    }
+
+    private void enableLogging() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+}
