cbd2bf0d770c98af6a8f7b76587771d870a52577
Filipe Bezerra <filipebzerra@gmail.com>
2016-01-04 00:16:12 -0200

    Removing unnecessary back stack listener and save and restore state of the last fragment in container.

--- app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/base/BaseActivity.java
+++ app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/base/BaseActivity.java
@@ -9,7 +9,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentManager.OnBackStackChangedListener;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
@@ -39,8 +38,6 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     protected MaterialDialogHelper mMaterialDialogHelper;
 
-    protected boolean mIsFragmentRestored = false;
-
     @Nullable @Bind(R.id.toolbar) protected Toolbar mToolbarActionBar;
 
     @Override
@@ -50,18 +47,6 @@ public abstract class BaseActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setupToolbarAsActionBar();
         mMaterialDialogHelper = MaterialDialogHelper.toContext(this);
-
-        if (inState != null) {
-            Timber.d("Activity restored state");
-
-            final Fragment fragmentSaved = getSupportFragmentManager()
-                    .getFragment(inState, STATE_FRAGMENT_IN_CONTAINER);
-
-            if (fragmentSaved != null) {
-                Timber.d("Fragment restored from activity state");
-                mIsFragmentRestored = replaceFragment(fragmentSaved, true) >= 0;
-            }
-        }
     }
 
     private void setupToolbarAsActionBar() {
@@ -139,24 +124,6 @@ public abstract class BaseActivity extends AppCompatActivity {
         Timber.d("Transaction commit with id %d", id);
 
         return id;
-    }
-
-    public void addOnBackStackChangedListener(OnBackStackChangedListener listener) {
-        getSupportFragmentManager().addOnBackStackChangedListener(listener);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        final Fragment fragmentFound = getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_placeholder);
-
-        if (fragmentFound != null) {
-            Timber.d("Saving fragment in container to activity state");
-            getSupportFragmentManager()
-                    .putFragment(outState, STATE_FRAGMENT_IN_CONTAINER, fragmentFound);
-        }
     }
 
     public void changeTitleAndSubtitle(String newTitle, String subtitle) {
