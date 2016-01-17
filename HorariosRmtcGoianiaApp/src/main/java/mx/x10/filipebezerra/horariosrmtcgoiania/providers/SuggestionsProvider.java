397b5e9bf18e1e35c8e130f9205ac8c5d1e6416e
Filipe Bezerra <filipebzerra@gmail.com>
2016-01-17 15:08:53 -0200

    Configuração com buildType para desenvolvimento e produção
    Esta configuração permite múltiplas instalações do app, assim não é
    preciso remover a versão de produção enquanto em desenvolvimento.

--- HorariosRmtcGoianiaApp/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/providers/SuggestionsProvider.java
+++ HorariosRmtcGoianiaApp/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/providers/SuggestionsProvider.java
@@ -1,14 +1,17 @@
 package mx.x10.filipebezerra.horariosrmtcgoiania.providers;
 
 import android.content.SearchRecentSuggestionsProvider;
+import mx.x10.filipebezerra.horariosrmtcgoiania.BuildConfig;
 
 /**
  * @author Filipe Bezerra
+ * @version 2.3.1, 17/01/2016
  * @since 2.0
  */
 public class SuggestionsProvider extends SearchRecentSuggestionsProvider {
-
-    public static final String AUTHORITY = SuggestionsProvider.class.getName();
+    public static final String AUTHORITY = BuildConfig.DEBUG ?
+            "mx.x10.filipebezerra.horariosrmtcgoiania.dev.providers.SuggestionsProvider" :
+            "mx.x10.filipebezerra.horariosrmtcgoiania.providers.SuggestionsProvider";
     public static final int MODE = DATABASE_MODE_QUERIES;
 
     public SuggestionsProvider() {
