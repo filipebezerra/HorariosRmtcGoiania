f4d0cbcd835ce1fc5fef2d0845f6c08855fdfbb8
Filipe Bezerra <filipebzerra@gmail.com>
2016-04-03 14:06:54 -0300

    Resolves #112

--- app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/home/FavoritesFragment.java
+++ app/src/main/java/mx/x10/filipebezerra/horariosrmtcgoiania/home/FavoritesFragment.java
@@ -1,10 +1,14 @@
 package mx.x10.filipebezerra.horariosrmtcgoiania.home;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 import mx.x10.filipebezerra.horariosrmtcgoiania.R;
 
@@ -16,6 +20,9 @@ import mx.x10.filipebezerra.horariosrmtcgoiania.R;
  * @since 3.0.0
  */
 public class FavoritesFragment extends Fragment {
+    @Bind(R.id.empty_state) LinearLayout mEmptyStateView;
+    @Bind(android.R.id.list) RecyclerView mRecyclerView;
+
     public static FavoritesFragment newInstance(){
         return new FavoritesFragment();
     }
@@ -26,5 +33,13 @@ public class FavoritesFragment extends Fragment {
         final View rootView = inflater.inflate(R.layout.fragment_favorites, container, false);
         ButterKnife.bind(this, rootView);
         return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mEmptyStateView.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.GONE);
     }
 }
